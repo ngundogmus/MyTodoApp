dfa3f2bc3ed50bd7d58e75743440bebd9a7d9cd3
ngundogmus <niyazigun@gmail.com>
2023-11-14 14:57:05 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {TodoWrapper} from './components/TodoWrapper';
+import {TodoForm} from './components/TodoForm';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App"> 
+      <TodoWrapper /> 
+    </div>
+  );
+}
+export default App;
+
+
+
+
